c7d1b5abd53f979e664418a81b4c53bad44fa14e
Louie Camacho <louiecamacho@Elizabeths-MacBook-Air.local>
2020-11-03 14:31:45 -0600

    more form fields

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const titleField = document.querySelector(
-	'input[name="title"]'
-);
+const titleField = document.querySelector('input[name="title"]');
 const form = document.querySelector('form');
 const booksList = document.querySelector('#booksList');
 
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, year) {
 	// the constructor...
+	this.title = title;
+	this.author = author;
+	this.year = year;
+	console.log(title, author, year);
 }
 
 function addBookToLibrary(elm) {
 	// do stuff here
 	myLibrary.push(elm);
-	const newItem = document.createElement('li');
-	booksList.appendChild(newItem).innerHTML += myLibrary;
+	booksList.innerHTML = myLibrary.join(', ');
 }
 
 form.addEventListener('submit', (e) => {
