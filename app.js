65330d3da15e7410b43753e2c94cbe5b54e4e8d5
Louie Camacho <louiecamacho@gmail.com>
2020-12-15 09:04:31 -0600

    adds single book to booksList

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-let myLibrary = [];
+let myLibrary = [
+	{
+		title: 'title',
+		author: 'author',
+		year: 'year',
+	},
+];
+const booksList = document.querySelector('#booksList');
 
 // Book.prototype.info = () => {
 // 	return `${title}, ${author}, ${year}`;
@@ -19,6 +26,20 @@ function addBookToLibrary() {
 	// store the new book objects into an array
 	const newBook = new Book(title, author, year);
 	myLibrary.push(newBook);
+
+	myLibrary.forEach((book) => {
+		booksList.innerHTML = `<table>
+		<tr>
+			<td>${book.title}</td>
+		</tr>
+		<tr>
+			<td>${book.author}</td>
+		</tr>
+		<tr>
+			<td>${book.year}</td>
+		</tr>
+	</table>`;
+	});
 }
 
 const form = document.querySelector('form');
@@ -26,5 +47,5 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 	addBookToLibrary();
-	form.reset();
+	// form.reset();
 });
