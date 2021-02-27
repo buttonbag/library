be9ffffe12b199f590a192fec8b5b31992351a45
louie.camacho <louie.camacho@hogarth-ogilvy.com>
2021-02-27 12:16:01 -0600

    new comments

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-let myLibrary = [
-	{
-		title: 'title',
-		author: 'author',
-		year: 'year',
-	},
-];
+const userTitle = document.querySelector('[name="title"]');
+const userAuthor = document.querySelector('[name="author"]');
+const userYear = document.querySelector('[name="year"]');
+const button = document.querySelector('button');
+
 const booksList = document.querySelector('#booksList');
 
-// Book.prototype.info = () => {
-// 	return `${title}, ${author}, ${year}`;
-// };
+let myLibrary = [];
 
+//constructor
 function Book(title, author, year) {
-	// the constructor...
 	this.title = title;
 	this.author = author;
 	this.year = year;
 }
 
-function addBookToLibrary() {
-	const title = document.querySelector('#title').value;
-	const author = document.querySelector('#author').value;
-	const year = document.querySelector('#year').value;
+// uses constructor to add new book to the array
+function addToLibrary() {
+	const book = new Book(userTitle.value, userAuthor.value, userYear.value);
+	myLibrary.push(book);
+	displayBooks();
+}
 
-	// store the new book objects into an array
-	const newBook = new Book(title, author, year);
-	myLibrary.push(newBook);
+button.addEventListener('click', (e) => {
+	e.preventDefault(); //prevents refreshing form on click
+	addToLibrary();
+});
 
+function test() {
+	alert('triggered');
+}
+
+// function loops through the array
+function displayBooks() {
+	booksList.innerHTML = ' '; // clears page first
 	myLibrary.forEach((book) => {
-		booksList.innerHTML = `<table>
-		<tr>
-			<td>${book.title}</td>
-		</tr>
-		<tr>
-			<td>${book.author}</td>
-		</tr>
-		<tr>
-			<td>${book.year}</td>
-		</tr>
-	</table>`;
+		// display each book on the page
+		booksList.innerHTML += `
+		<table class="book">
+			<tr><td>${book.title}</td></tr>
+			<tr><td>${book.author}</td></tr>
+			<tr><td>${book.year}</td></tr>
+		</table>`;
 	});
 }
 
-const form = document.querySelector('form');
+// Add a button on each book’s display to remove the book from the library.
 
-form.addEventListener('submit', function (e) {
-	e.preventDefault();
-	addBookToLibrary();
-	// form.reset();
-});
+// Add a button on each book’s display to change its read status.
+
+// optional: add localStorage
+// optional: add firebase
