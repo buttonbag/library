f755d4671e62ad04e8d21c451a21c0e233352cc4
louie.camacho <louie.camacho@hogarth-ogilvy.com>
2021-02-27 13:11:23 -0600

    added read status

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const userTitle = document.querySelector('[name="title"]');
 const userAuthor = document.querySelector('[name="author"]');
 const userYear = document.querySelector('[name="year"]');
+const userRead = document.querySelector('[name="read"]');
 const button = document.querySelector('button');
 
 const booksList = document.querySelector('#booksList');
@@ -8,26 +9,23 @@ const booksList = document.querySelector('#booksList');
 let myLibrary = [];
 
 //constructor
-function Book(title, author, year) {
+function Book(title, author, year, read) {
 	this.title = title;
 	this.author = author;
 	this.year = year;
+	this.read = read;
 }
 
 // uses constructor to add new book to the array
 function addToLibrary() {
-	const book = new Book(userTitle.value, userAuthor.value, userYear.value);
+	const book = new Book(
+		userTitle.value,
+		userAuthor.value,
+		userYear.value,
+		userRead.checked
+	);
 	myLibrary.push(book);
 	displayBooks();
-}
-
-button.addEventListener('click', (e) => {
-	e.preventDefault(); //prevents refreshing form on click
-	addToLibrary();
-});
-
-function test() {
-	alert('triggered');
 }
 
 // function loops through the array
@@ -37,12 +35,18 @@ function displayBooks() {
 		// display each book on the page
 		booksList.innerHTML += `
 		<table class="book">
-			<tr><td>${book.title}</td></tr>
-			<tr><td>${book.author}</td></tr>
-			<tr><td>${book.year}</td></tr>
+		<tr><td>${book.title}</td></tr>
+		<tr><td>${book.author}</td></tr>
+		<tr><td>${book.year}</td></tr>
+		<tr><td>${book.read}</td></tr>
 		</table>`;
 	});
 }
+
+button.addEventListener('click', (e) => {
+	e.preventDefault(); //prevents refreshing form on click
+	addToLibrary();
+});
 
 // Add a button on each book’s display to remove the book from the library.
 
